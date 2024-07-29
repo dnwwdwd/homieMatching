5dff7c97ee55d44d3b2b9d702a75bc6654d94875
dnwwdwd <1765393959@qq.com>
2024-07-29 23:04:19 +0800

    add 查询登录用户点赞博客数、收藏博客数、粉丝和发布博客数

---
+++ src/main/java/com/hjj/homieMatching/mapper/BlogMapper.java
@@ -0,0 +1,18 @@
+package com.hjj.homieMatching.mapper;
+
+import com.hjj.homieMatching.model.domain.Blog;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author hejiajun
+* @description 针对表【blog(博客表)】的数据库操作Mapper
+* @createDate 2024-07-17 16:25:17
+* @Entity com.hjj.homieMatching.model.domain.Blog
+*/
+public interface BlogMapper extends BaseMapper<Blog> {
+
+}
+
+
+
+
---
+++ src/main/java/com/hjj/homieMatching/mapper/FollowMapper.java
@@ -0,0 +1,18 @@
+package com.hjj.homieMatching.mapper;
+
+import com.hjj.homieMatching.model.domain.Follow;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 何佳骏
+* @description 针对表【follow(关注表)】的数据库操作Mapper
+* @createDate 2024-07-19 12:46:18
+* @Entity com.hjj.homieMatching.model.domain.Follow
+*/
+public interface FollowMapper extends BaseMapper<Follow> {
+
+}
+
+
+
+
